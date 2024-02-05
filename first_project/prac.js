8863ef441c347d075461e4842813a962edf61ebb
hyojin <dhflekddy@hanmail.net>
2024-02-05 19:21:12 +0900

    refined

--- first_project/prac.js
+++ first_project/prac.js
@@ -1,65 +1,68 @@
+let arr=[1,2,3,4];
+console.log(Array(9));
+let result=Array(9).fill().map((value, idx)=>{
+    return idx;
+});
+console.log(result);
+
 // const fs=require('fs');
 // const filepath=process.platform==='linux'?'dev/stdin':'./input.txt';
-// let input=fs.readFileSync(filepath).toString().split("\n");
-// input[0]=input[0].split(" ").map(item=>+item);
-// let n=input[0][0];
-// const target=input[0][1];
-// const arr=input[1].split(" ").map(item=>+item);
+// const input = fs.readFileSync(filepath).toString().trim().split("\n");
 
-// solution(n);
+// input[0]=input[0].split(" ").map(n=>+n);
+// const node=input[0][0];
+// const times=input[0][1];
 
-// function solution(n){
-//   let answer=0;
-//   for(let i=0;i<n-2;++i){
-//     for(let j=i+1;j<n-1;++j){
-//       for(let k=j+1;k<n;++k){
-//         let temp=arr[i]+arr[j]+arr[k];
-//         if(temp<=target && temp>answer)answer=temp;
-//       }
-//     }
+// class Edge{
+//   constructor(from, to){
+//     this.from=from;
+//     this.to=to;
 //   }
-//   console.log(answer);
 // }
 
-// let x=5;
+// class UnionFind{
+//   constructor(n){
+//     this.parent=new Array(n);// Js에서는 그냥 this.parent=[]; 로만해도 아래 인덱스 다 적용됨.
+//     this.size=new Array(n).fill(0);
+//     for(let i=0;i<n;++i){
+//       this.parent[i]=i;
+//     }
+//   }
 
+//   find(x){
+//     if(x==this.parent[x])return x;
+//     return this.parent[x]=this.find(this.parent[x]);
+//   }
+//   union(x1, x2){
+//     if(this.size[x1]<this.size[x2]){
+//       this.size[x2]+=this.size[x1];
+//       this.size[x1]=0;
+//       this.parent[x1]=x2;
+//     }
+//     else{
+//       this.size[x1]+=this.size[x2];
+//       this.size[x2]=0;
+//       this.parent[x2]=x1;
+//     }
+//   }
+// }
 
-
-
-
-
-const fs=require('fs');
-const { arrayBuffer } = require('stream/consumers');
-const filepath=process.platform==='linux'?'dev/stdin':'./input.txt';
-let input=fs.readFileSync(filepath).toString().split("\n");
-
-input[0]=input[0].split(" ").map(ele=>+ele);
-const n=input[0][0];
-const arr=new Array(n);
-const target=input[0][1];
-for(let i=0;i<n;++i){
-    arr[i]=input[i+1].trim();
-}
-console.log(arr)
-
-solution();
-
-function solution(){
-    let dp=new Array(target+1).fill(0);
-    dp[0]=1;
-  // for(i in arr){
-    //     for(j in Range(i, target+1)){
-    //         dp[j]+=dp[j-i];
-    //     }
-    // }
-  
-
-    for(let i=0;i<arr.length;++i){
-        for(let j=arr[i];j<target+1;++j){
-            dp[j]+=dp[j-arr[i]];
-        }
-    }
-    console.log(dp[target]);
-}
-
-
+// let edges=[];
+// for(let i=1;i<=times;++i){
+//   input[i]=input[i].split(" ").map(num=>+num);
+//   edges.push(new Edge(input[i][0], input[i][1]));
+// }
+// let answer=0;
+// const uf=new UnionFind(node);
+// for(let i=0;i<times;++i){
+//   let parentOfP1=uf.find(edges[i].from);
+//   let parentOfP2=uf.find(edges[i].to);
+//   if(parentOfP1!==parentOfP2){
+//     uf.union(parentOfP1, parentOfP2);
+//   }
+//   else{
+//     console.log(i+1);
+//     return;
+//   }
+// }
+// console.log(0);
